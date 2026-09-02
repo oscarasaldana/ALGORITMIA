f128c6798950d386c001f6edd5522b0d60bb69e3
Oscar Saldaña <oscarasaldana.6@gmail.com>
2026-09-02 10:04:03 -0500

    Add header guard and function declarations in funciones.h

--- Laboratorio1/2025(2)/pregunta2/funciones.h
+++ Laboratorio1/2025(2)/pregunta2/funciones.h
@@ -1,1 +1,12 @@
 
+#ifndef PREGUNTA2_FUNCIONES_H
+#define PREGUNTA2_FUNCIONES_H
+
+const int fila = 6;
+const int columna = 11;
+
+void mostrarMina(int (*)[columna]);
+bool recursion(int (*)[columna], int , int , int );
+void mostrarCaminos(int (*)[columna]);
+
+#endif //PREGUNTA2_FUNCIONES_H
